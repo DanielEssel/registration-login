e973ac662dc023fbd0056ec7c318a15142a15ad7
DanielEssel <dessel731@outlook.com>
2024-09-17 15:43:39 +0000

    add values

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,22 @@
 // Get form element by id
-const registerForm = document.getElementById("register");
+const registerForm = document.querySelector("#register");
 // Handle form submit event
-registerForm.onsubmit = function (event) {
+registerForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log(event);
-};
+  // Collect from data
+  const formData = new FormData(registerForm);
+  // console.log(formData.get('username'), formData.get('email'), formData.get('password'));
+  // Send data to backend (will do that tomorrow)
+  // Display success message to user (u have to create h1 in the form section and use id selector "message") NB;The space between the h1 created in html is called 'inner html'
+  const messageH1 = document.querySelector("#message");
+  messageH1.textContent = "Account registered successfully!!";
+});
+// declare variable age
+let myAge = 32;
+// Define squareAge function
+function squareAge(age) {
+  return age ** 2;
+}
+
+const squaredAge = squareAge(32);
+console.log(squaredAge);
